38854a0a1aa3a4758c9733170ed90e7cb5ff2229
mk499490 <mk499490@gmail.com>
2016-02-10 16:43:19 +0900

    Added lobby menu
    Signed-off-by: mk499490 <mk499490@gmail.com>

--- src/com/horizonbagel/therun/Lobby.java
+++ src/com/horizonbagel/therun/Lobby.java
@@ -3,13 +3,17 @@ package com.horizonbagel.therun;
 import net.minecraft.server.v1_8_R3.CommandTellRaw;
 import net.minecraft.server.v1_8_R3.IChatBaseComponent;
 import net.minecraft.server.v1_8_R3.PacketPlayOutTitle;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class Lobby implements Listener{
@@ -29,10 +33,35 @@ public class Lobby implements Listener{
         //player.getInventory().setItem(0, mapSelector);
         //itemMeta.setDisplayName(name);
 
-        ItemStack obsidian = new ItemStack(Material.OBSIDIAN, 64);
+        // ロビーアイテムの定義。(§は「セクション」と言う。)
+        ItemStack mapSelector = new CustomItem(Material.CHAINMAIL_HELMET, "§e§lMap Selector");
+        ItemStack itemBox = new CustomItem(Material.CHEST, "§e§lItem");
+        ItemStack store = new CustomItem(Material.EMERALD, "§e§lStore");
+        ItemStack settings = new CustomItem(Material.ANVIL, "§e§lSettings");
+        ItemStack gameMasterTool = new CustomItem(Material.COMMAND, "§e§lGame Master Tool");
 
-        ItemStack mapSelector = new CustomItem(Material.ENDER_PORTAL_FRAME, "§e§lMap Selector");
+        // ロビーアイテムの付与処理
         player.getInventory().setItem(0, mapSelector);
+        player.getInventory().setItem(2, itemBox);
+        player.getInventory().setItem(3, store);
+        player.getInventory().setItem(5, settings);
 
+        if (player.isOp()) {
+            player.getInventory().setItem(7, gameMasterTool);
+        }
+
+    }
+
+    @EventHandler
+    public void ballFiring(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (((event.getAction() == Action.RIGHT_CLICK_AIR) &&
+                (player.getItemInHand().getType().equals(Material.CHAINMAIL_HELMET)))) {
+            Inventory gameRoomSelectorGUI;
+            ItemStack cancel = new CustomItem(Material.BARRIER, "§c§lCancel");
+            gameRoomSelectorGUI = Bukkit.createInventory(null, 9, "Map Selector");
+            gameRoomSelectorGUI.setItem(8, cancel);
+            player.openInventory(gameRoomSelectorGUI);
+        }
     }
 }
